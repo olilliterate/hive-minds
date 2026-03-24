10428ca7ee0c585d58a6aee7af58ea1bc031c20d
olilliterate <oliver@bjerg.co.uk>
2026-03-24 16:54:41 +0000

    Completed result controller tests

--- server/controllers/result.js
+++ server/controllers/result.js
@@ -3,9 +3,9 @@ const Result = require("../models/Result");
 const index = async (req, res) => {
     try{
         const resultsData = await Result.getAll();
-        res.status(200).send(resultsData);
+        res.status(200).send({ data: resultsData });
     } catch (err) {
-        res.status(500).send({error: err.message })
+        res.status(500).send({ error: err.message });
     }
 }
 
@@ -13,9 +13,9 @@ const postResult = async (req, res) => {
     const data = req.body
     try{
         const postedRecord = await Result.create(data);
-        res.status(201).send(postedRecord)
+        res.status(201).send( {data: postedRecord} );
     } catch (err) {
-        res.status(500).send({ error: err.message })
+        res.status(400).send({ error: err.message })
     }
 }
 
