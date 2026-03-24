1c77bd0be5bdac28554542814db07826040c137c
olilliterate <oliver@bjerg.co.uk>
2026-03-24 12:02:27 +0000

    Finished the Result model tests

--- server/models/Result.js
+++ server/models/Result.js
@@ -1,27 +1,36 @@
 const db = require('../db/connect')
 class Result {
-    constructor({name, streak}) {
+    constructor({id, name, streak, date, user_id}) {
+        this.id = id;
         this.name = name;
         this.streak = streak;
+        this.date = date;
+        this.user_id = user_id;
     }
-    static async getAllResults() {
-        try{
-            const resp = await db.query("SELECT user_data.name, result.streak FROM result LEFT JOIN user_date ON (user_data.user_id = result.user_id);");
-            return resp.rows.map((ele) => new Result(ele));
-        } catch (err) {
-            throw new Error("Unable to fetch results")
+    static async getAll() {
+        
+        const resp = await db.query("SELECT user_data.name, result.streak, result.id, result.date, result.user_id FROM result LEFT JOIN user_date ON (user_data.user_id = result.user_id);");
+        if (resp.rows.length === 0) {
+            throw new Error("No results available.")
         }
+        return resp.rows.map((ele) => new Result(ele));
 
     }
 
-    static async createResult(user_id, streak) {
-        try {
-            const resp = await db.query("INSERT INTO result (user_id, streak, date) VALUES ($1, $2, $3) RETURNING RETURNING *", [user_id, streak, Date.now()]);
-            return resp
-        } catch (err) {
-            throw new Error("Error uploading your result")   ;
+    static async create(data) {
+    
+        if (!data.user_id) { 
+            throw new Error("user_id is missing") 
         }
+
+        if (!data.streak) {
+        throw new Error("streak is missing")
+        }
+        
+        const resp = await db.query("INSERT INTO result (user_id, streak, date) VALUES ($1, $2, $3) RETURNING *", [data.user_id, data.streak, new Date().toISOString()]);
+        return new Result(resp.rows[0]);
     }
 }
+
 
 module.exports = Result;
