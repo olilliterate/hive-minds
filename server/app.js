4207f4883963a61f72376b5f89d6eb7db7845d36
ShubhraTripathi-tech <shubhra.tripathi1611@gmail.com>
2026-03-25 12:59:14 +0000

    Added landing page, dashboard and logout

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const userRouter = require("./routes/user");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client")));
 app.use("/users", userRouter);
 
 app.get("/", (req, res) => {
-  res.json({ message: "API is running 🚀" });
+  res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
 module.exports = app;
