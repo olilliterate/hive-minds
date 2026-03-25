c7e4d46bd6be069d42bbf0b2cea72e4763bb34f0
Obi Ojji <obi.ojji@lafosseacademy.com>
2026-03-25 17:57:57 +0000

    eventlistener on click working

--- client/assets/game.js
+++ client/assets/game.js
@@ -1,9 +1,10 @@
 // LEt's get ready to ruuuuummmmbbbbbllleeeee!!!!
 
-const {startGameLoop} = require("../gameLoop")
+//const {startGameLoop} = require("../gameLoop")
 
 document.querySelector("#startGame").addEventListener("click", () => {
     document.querySelector(".game-board").classList.remove("d-none")
+    console.log("I work right")
     startGameLoop()
 })
 
