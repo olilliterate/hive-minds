67dcf498d13e93da46eda0691f35a00e06619c75
olilliterate <oliver@bjerg.co.uk>
2026-03-26 10:36:21 +0000

    Updated randomChoice tests

--- __tests__/unit/helpers/randomChoice.test.js
+++ __tests__/unit/helpers/randomChoice.test.js
@@ -2,6 +2,12 @@ const randomChoice = require("../../../helpers/randomChoice");
 
 describe('randomChoice', () => {
     it("returns an element of an array when passed an array", () => {
-        
+        //Arrange
+        testArray = [2,1,5,3,1,2]
+        //Act
+        const testValue = randomChoice(testArray);
+        //Assert
+        expect(testValue).toBeGreaterThan(0);
+
     })
 })
